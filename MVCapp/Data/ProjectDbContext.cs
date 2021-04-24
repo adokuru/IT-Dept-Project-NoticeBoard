38d93a952a0c03e9d72ec860c208986643acae20
David Ghena <davidadokuru@gmail.com>
2021-04-24 15:53:52 +0100

    Projects Setup

---
+++ MVCapp/Data/ProjectDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCapp.Data
+{
+    public class ProjectDbContext :DbContext 
+    {
+        public ProjectDbContext(DbContextOptions<ProjectDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Project> Projects { get; set; }
+    }
+}
